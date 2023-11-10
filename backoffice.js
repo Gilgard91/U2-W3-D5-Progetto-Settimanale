d6899417caf8c4bcc91594dbc5361bcb458f0ea2
Giordano <zagattigiordano@hotmail.it>
2023-11-10 15:18:17 +0100

    edit

--- backoffice.js
+++ backoffice.js
@@ -18,13 +18,15 @@ window.onload = () => {
     })
       .then((resp) => resp.json())
       .then((editObj) => {
-        const { name, imageUrl, description, price } = editObj;
+        const { name, brand, imageUrl, description, price } = editObj;
 
         document.getElementById("name").value = name;
+        document.getElementById("brand").value = brand;
         document.getElementById("image").value = imageUrl;
         document.getElementById("description").value = description;
         document.getElementById("price").value = price;
-      });
+      })
+      .catch((err) => console.log(err));
 
     document.getElementById("backoffice-title").innerText = "Modifica articolo";
     const btn = document.querySelector("button[type='submit']");
@@ -38,27 +40,50 @@ window.onload = () => {
   }
 };
 
-// const createArticle = (event) => {
-//   event.preventDefault();
+const handleSubmit = (event) => {
+  event.preventDefault();
 
-//   const articleObj = {
-//     name: document.getElementById("name").value,
-//     imageUrl:
-//       "https://st3.depositphotos.com/1000212/16750/v/450/depositphotos_167507880-stock-illustration-musical-instrument-grand-piano.jpg",
-//     description: document.getElementById("description").value,
-//     price: document.getElementById("price").value
-//   };
+  const articleObj = {
+    name: document.getElementById("name").value,
+    brand: document.getElementById("brand").value,
+    imageUrl:
+      "https://st3.depositphotos.com/1000212/16750/v/450/depositphotos_167507880-stock-illustration-musical-instrument-grand-piano.jpg",
+    description: document.getElementById("description").value,
+    price: document.getElementById("price").value
+  };
 
-//   fetch(URL, {
-//     method,
-//     body: JSON.stringify(articleObj),
-//     headers: {
-//       Authorization:
-//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWYwMzI1NGU4ODAwMTgzZjE4OGUiLCJpYXQiOjE2OTk2MDYyNzUsImV4cCI6MTcwMDgxNTg3NX0.djs-nQQziVGfyHVb7gGqd4aR2i_Q7DMGdTZV2cUH6g0",
-//       "Content-Type": "application/json"
-//     }
-//   })
-//     .then((resp) => resp.json())
-//     .then((createdObj) => {});
+  fetch(URL, {
+    method,
+    body: JSON.stringify(articleObj),
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWYwMzI1NGU4ODAwMTgzZjE4OGUiLCJpYXQiOjE2OTk2MDYyNzUsImV4cCI6MTcwMDgxNTg3NX0.djs-nQQziVGfyHVb7gGqd4aR2i_Q7DMGdTZV2cUH6g0",
+      "Content-Type": "application/json"
+    }
+  }).then((resp) => resp.json());
 
-// };
+  console.log(articleObj);
+
+  if (!pianoId) {
+    document.getElementById("name").value = "";
+    document.getElementById("brand").value = "";
+
+    document.getElementById("description").value = "";
+    document.getElementById("price").value = "";
+  }
+};
+
+const deleteArticle = () => {
+  const hasAccepted = confirm(
+    "Sei sicuro di voler eliminare l'articolo? L'azione è irreversibile."
+  );
+
+  if (hasAccepted) {
+    fetch(URL, { method: "DELETE" })
+      .then((resp) => resp.json())
+      .then((deletedObj) => {
+        alert("Hai eliminato " + deletedObj.name);
+        window.location.assign("./index.html");
+      });
+  }
+};
